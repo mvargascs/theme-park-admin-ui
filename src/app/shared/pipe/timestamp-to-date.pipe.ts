df9cec7b897eb1ac8600ef531c3ef0a386bf764e
Miguel Vargas <mvargascswork@gmail.com>
2020-03-28 13:44:40 -0400

    Updated TimestampToDatePipe to check incoming value is an instance of Timestamp.

--- src/app/shared/pipe/timestamp-to-date.pipe.ts
+++ src/app/shared/pipe/timestamp-to-date.pipe.ts
@@ -1,13 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { firestore } from 'firebase';
+import * as firebase from 'firebase/app';
+import Timestamp = firebase.firestore.Timestamp;
 
 @Pipe({
   name: 'timestampToDate'
 })
 export class TimestampToDatePipe implements PipeTransform {
 
-  transform(value: firestore.Timestamp, ...args: any[]): any {
-    return value.toDate();
+  transform(value: Timestamp): Date {
+    if (value instanceof Timestamp) {
+      return value.toDate();
+    } else {
+      return value;
+    }
   }
-
 }
