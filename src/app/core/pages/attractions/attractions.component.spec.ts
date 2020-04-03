58969d9ba5f4a5578422684bd43ef5ed2af6e7c6
Miguel Vargas <mvargascswork@gmail.com>
2020-04-03 11:46:25 -0400

    Added mat menu dependency.

--- src/app/core/pages/attractions/attractions.component.spec.ts
+++ src/app/core/pages/attractions/attractions.component.spec.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 
@@ -40,6 +41,7 @@ describe('AttractionsComponent', () => {
         MatCardModule,
         MatIconModule,
         MatInputModule,
+        MatMenuModule,
         MatSortModule,
         MatTableModule,
         NoopAnimationsModule,
