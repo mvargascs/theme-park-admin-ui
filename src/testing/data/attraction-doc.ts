88cf5aa38612992bbcf68f709b0c6a5f04fb39cd
Miguel Vargas <mvargascswork@gmail.com>
2020-04-03 11:18:41 -0400

    linted attraction doc model.

--- src/testing/data/attraction-doc.ts
+++ src/testing/data/attraction-doc.ts
@@ -1,10 +1,10 @@
 import { attractions } from './attractions';
 
 export class AttractionDocument {
-    
+
     constructor() {}
-    
+
     data(): any {
         return attractions[0];
     }
-};
+}
